57d55e56530ca02cb56171e925918e11dd541e84
IBrickhouse <mrfurbyb@gmail.com>
2024-06-19 22:13:59 -0700

    Add regex to filter out only words that begin with capital letters

--- script.js
+++ script.js
@@ -9,12 +9,24 @@ async function performOCR() {
 
     const imageFile = imageInput.files[0];
     const { data: { text } } = await Tesseract.recognize(imageFile);
-    let splitText = text.split(/\W+/);
-    
-    //remove non english elements
+    // Remove non word elements
+    var splitText = text.split(/\W+/);
+    //var splitText = text;
+
+    // Remove non english elements
     splitText.forEach((element, idx) => {
-        splitText[idx] = element.replace(/[^a-zA-Z ]/g, '');
+        //replace non letters
+        //splitText[idx] = element.replace(/[0-9]/g, '')
+
+        //Remove non english
+        //splitText[idx] = element.replace(/[^a-zA-Z]/g, '');
+
+        //replace all elements that dont begin with capitalized letters
+        splitText[idx] = element.replace(/\b[a-z0-9][a-zA-Z0-9]*\b/, '');
        });
+    // Filter out empty elements
+    splitText = splitText.filter(Boolean)
+
 
     // Generate a ramdom number beteen 0 and the length of the list of elements
     var randomNumber = Math.floor(Math.random() * splitText.length);
