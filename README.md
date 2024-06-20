35a343fe07d02bd4069ef3c4a55a9ac28f76c72e
IBrickhouse <mrfurbyb@gmail.com>
2024-06-19 18:16:50 -0700

    Add Single English Word Selection

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Web App Text Decider
+Ever have truble deciding what to have on a large menu? Use this tool to choose for you!
+
+Special thanks to https://github.com/Hardik-369/OCR_RECOGNITION_WEB_APP for the starting code
--- script.js
+++ script.js
@@ -9,5 +9,19 @@ async function performOCR() {
 
     const imageFile = imageInput.files[0];
     const { data: { text } } = await Tesseract.recognize(imageFile);
-    resultElement.textContent = 'Extracted Text: ' + text;
+    let splitText = text.split(/\W+/);
+    
+    //remove non english elements
+    splitText.forEach((element, idx) => {
+        splitText[idx] = element.replace(/[^a-zA-Z ]/g, '');
+       });
+
+    // Generate a ramdom number beteen 0 and the length of the list of elements
+    var randomNumber = Math.floor(Math.random() * splitText.length);
+    var randomChoice = splitText[randomNumber];
+    
+    resultElement.textContent = 'You should try the ' + randomChoice;
+
+    console.log(randomChoice + ' was chosen at index ' + randomNumber)
+    console.log(splitText);
 }
